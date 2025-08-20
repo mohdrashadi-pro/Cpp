9dcf6665a6fb19cd88c5a35a5100cca3cee2d8d8
mohdrashadi-pro <moalrashadi@gmail.com>
2025-08-20 18:35:52 +0800

    Remove old introduction folder after renaming to 0-introduction

--- introduction/2.cpp
+++
@@ -1,9 +0,0 @@
-/*
-  iostream Header File Content
-  -- cout   // Standard output stream
-  -- cin    // Standard input stream
-  -- cerr   // Standard error stream (unbuffered)
-  -- clog   // Standard logging stream (buffered)
-  -- <<     // Insertion operator
-  -- >>     // Extraction operator
-*/
