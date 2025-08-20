6a0f9770ea732979dca36466fe8605eadc9a0ace
mohdrashadi-pro <moalrashadi@gmail.com>
2025-08-20 18:23:43 +0800

    i'm finish only 1 - > 3 cpp file

---
+++ introduction/2.cpp
@@ -0,0 +1,9 @@
+/*
+  iostream Header File Content
+  -- cout   // Standard output stream
+  -- cin    // Standard input stream
+  -- cerr   // Standard error stream (unbuffered)
+  -- clog   // Standard logging stream (buffered)
+  -- <<     // Insertion operator
+  -- >>     // Extraction operator
+*/
